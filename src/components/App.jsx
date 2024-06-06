e683d32cda51bb326d607bfb6b0620327f95f802
Anurag Papolu <anurag.papolu@gmail.com>
2024-06-06 01:18:06 -0400

    Fixing router issue

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,42 @@
 import React, { useRef } from "react";
 import { Routes, Route, Outlet, Navigate } from "react-router-dom";
-import GoogleAnalyticsTracker from '../components/GoogleAnalyticsTracker';
-import Intro from '../components/Intro';
-import Navbar from '../components/Navbar';
-import { Work, Education, Projects } from '../components/Experiences';
-import Footer from '../components/Footer';
-import '../scss/App.scss';
+import GoogleAnalyticsTracker from "../components/GoogleAnalyticsTracker";
+import Intro from "../components/Intro";
+import Navbar from "../components/Navbar";
+import { Work, Education, Projects } from "../components/Experiences";
+import Footer from "../components/Footer";
+import "../scss/App.scss";
 
 // Initialize routing and Google Analytics for page views
 export default function App() {
   return (
     <>
-      <GoogleAnalyticsTracker/>
+      <GoogleAnalyticsTracker />
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={ <Work /> } />
-          <Route path="education" element={ <Education />} />
-          <Route path="projects" element={ <Projects />} />
-          <Route path="*" element={ <Navigate to="/"/> } />
+          <Route index element={<Work />} />
+          <Route path="education" element={<Education />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </>
   );
 }
 
-// Initialize page layout 
+// Initialize page layout
 function Layout() {
-  const mainContentRef = useRef(null)
+  const mainContentRef = useRef(null);
   return (
     <>
       <div className="column">
-        <Intro mainContentRef={ mainContentRef } />
+        <Intro mainContentRef={mainContentRef} />
       </div>
-      <div ref= { mainContentRef } className="column main-content">
-        <Navbar mainContentRef={ mainContentRef }/>
-        <Outlet/>
-        <Footer/>
-       </div>
+      <div ref={mainContentRef} className="column main-content">
+        <Navbar mainContentRef={mainContentRef} />
+        <Outlet />
+        <Footer />
+      </div>
     </>
   );
 }
